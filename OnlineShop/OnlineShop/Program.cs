21e9f2e67e5b27e9994fece3dff346031913317e
Alin <alin_popescu02@yahoo.com>
2023-04-09 16:24:08 +0300

    Repo

--- OnlineShop/OnlineShop/Program.cs
+++ OnlineShop/OnlineShop/Program.cs
@@ -1,3 +1,5 @@
+using OnlineShop.Repositories.AddressRepository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
+
 
 var app = builder.Build();
 
